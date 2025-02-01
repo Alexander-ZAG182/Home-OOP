6ad99eb0d863d45b1f9fce378b986df06c5149b2
Alexander-ZAG182 <hado18190@gmail.com>
2025-02-01 18:18:58 +0300

    finishOOP3

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,11 +1,14 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.article.Article;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.SimpleProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.search.SearchEngine;
 
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
@@ -18,6 +21,7 @@ public class App {
         Product juice = new SimpleProduct("Сок", 350);
 
         ProductBasket basket = new ProductBasket();
+
         basket.addProduct(bike);
         basket.addProduct(apple);
         basket.addProduct(banana);
@@ -34,6 +38,32 @@ public class App {
         System.out.println("Стоимость пустой корзины : " + basket.sumBasket());
         System.out.println("Поиск товара : " + apple.getNameProduct() + " в корзине : " + basket.findProduct(apple.getNameProduct()));
 
+        SearchEngine searchEngine = new SearchEngine();
 
+        Article appleArticle = new Article(
+                "Статья о яблоках",
+                "Яблоко - очень полезно");
+        Article bikeArticle = new Article(
+                "Статья о велосипеде ",
+                "Велосипед - средство передвижения"
+        );
+
+        searchEngine.add(banana);
+        searchEngine.add(bike);
+        searchEngine.add(apple);
+        searchEngine.add(water);
+        searchEngine.add(cheese);
+        searchEngine.add(juice);
+        searchEngine.add(appleArticle);
+        searchEngine.add(bikeArticle);
+
+        String searchQuery1 = "Яблоко";
+        System.out.println("Поиск \"" + searchQuery1 + "\": " + Arrays.toString(searchEngine.search(searchQuery1)));
+
+        String searchQuery2 = "Велосипед";
+        System.out.println("Поиск \"" + searchQuery2 + "\": " + Arrays.toString(searchEngine.search(searchQuery2)));
+
+        String searchQuery3 = "Лодка";
+        System.out.println("Поиск \"" + searchQuery3 + "\": " + Arrays.toString(searchEngine.search(searchQuery3)));
     }
 }
